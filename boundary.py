539932e3e7f16efb14166fa03ef9011717d6a0a8
esheder <esheder@gmail.com>
2018-07-02 14:42:20 -0400

    Started boundary solve, added comments

--- boundary.py
+++ boundary.py
@@ -5,11 +5,18 @@
 
 import numpy as np
 from scipy.optimize import newton_krylov
+from scipy.linalg import sqrtm, expm
 
 def get_tallied(flx, D):
+    """Extracts tallied value rather than diffusion coefficient
+
+    """
     return 6*np.multiply(flx,D)
 
 def read_inp(f):
+    """Reads data files
+
+    """
     with open(f, 'r') as of:
         lines = of.readlines()
     n = len(lines)
@@ -22,9 +29,36 @@ def read_inp(f):
         sigS[i,:] = line.split()[3:]
     return sigT, sigS, flx, D
 
+def BCsolve(Up, Um, Dp, Dm, Jp, d):
+    """Solves the boundary interface problems
+
+    """
+    exUp = expm(-d*Up)
+    exUmp = expm(d*Um)
+    exUmm = expm(-d*Um)
+    n = exUp.shape[0]
+    b = np.zeros((3*n,1))
+    for i,j in enumerate(range(2*n,3*n)):
+        b[j] = Jp[i]
+    A = np.zeros((3*n,3*n))
+    for i,j in enumerate(range(0,n)):
+        for k,l in enumerate(range(0,n)):
+            A[j,l] = exUmm[i,k]
+        for k,l in enumerate(range(0,n)):
+            A[j,l] = exUmp[i,k]
+        for k,l in enumerate(range(0,n)):
+            A[j,l] = -exUp[i,k]
+            
+
 def solve(sigT, sigS, phi0, phi1, d, Jp):
+    """Solve for the diffusion coefficient using non-linear solvers
+
+    """
 
     def residual(D):
+        """Returns how close we are to convergence in each variable
+
+        """
         n = D.shape[0]
         Dm = np.zeros_like(sigT)
         Dp = np.zeros_like(sigT)
@@ -38,13 +72,12 @@ def solve(sigT, sigS, phi0, phi1, d, Jp):
         rm = Dm.dot(phi0m) - Dm.dot(d*exU.dot(Ap)) - phi1m
         rp = Dp.dot(phi0p) + Dp.dot(d*exU.dot(Ap)) - phi1p
         return np.concatenate(rm,rp)
-
+    
     guess = np.ones((Jp.shape[0],2))
     newton_krylov(residual, guess, method='lgmres')
 
 if __name__ == '__main__':
     from argparse import ArgumentParser as AP
-
 
     parser = AP(description="Solves the very specific problem")
     parser.add_argument('-i', default='data.dat', help="CMM tally results")
@@ -59,4 +92,3 @@ if __name__ == '__main__':
     Jp = np.zeros(flx.size)
     Jp[0] = 1.0
     D = solve(sigT, sigS, flx, rJ, d, Jp)
-    
