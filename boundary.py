411b84a4dad379850c429796d21d3eda67a2fd85
esheder <esheder@gmail.com>
2018-07-16 21:05:14 -0400

    First working version with input

--- boundary.py
+++ boundary.py
@@ -5,41 +5,8 @@
 
 import numpy as np
 from scipy.optimize import newton_krylov
-from scipy.sparse.linalg import aslinearoperator
 from scipy.linalg import sqrtm, expm, solve
-
-class BCCLS:
-    def __init__(self, exUp, exUmp, exUmm, Up, Um, Dp, Dm, d):
-        self.exUp = exUp
-        self.exUmp = exUmp
-        self.exUmm = exUmm
-        self.Up = Up
-        self.Um = Um
-        self.Dp = Dp
-        self.Dm = Dm
-        self.d = d
-        self.n = Dm.shape[0]
-        self.shape = (3*self.n,3*self.n)
-
-    def matvec(self, v):
-        if v.shape[0] != 3*self.n:
-            raise ValueError("Moo!")
-        
-        n = self.n
-        sol = np.zeros_like(v)
-        v1,v2,v3 = v[:n],v[n:2*n],v[2*n:]
-        sol[:n] = self.exUmm*v1 + self.exUmp*v2 - self.exUp*v3
-        sol[n:2*n] = self.Dm*(self.Um*(self.exUmp*v2 - self.exUmm*v1)) + self.Dp*self.Up*self.exUp*v3
-        sol[2*n:] = v1 + v2 - 3.0*self.Dm*self.Um*(v2 - v1)
-        return sol
-
-def linsolve(M,b):
-    A = aslinearoperator(M)
-    v, info = solve(A,b,)
-    if info == 0:
-        return v
-    else:
-        print(info)
+from util import *
 
 def get_tallied(flx, D):
     """Extracts tallied value rather than diffusion coefficient
@@ -55,23 +22,30 @@ def read_inp(f):
         lines = of.readlines()
     n = len(lines)
     sigS = np.zeros((n,n))
-    sigT = np.zeros((n,n))
-    D = np.zeros((n,1))
-    flx = np.zeros((n,1))
+    sigT = np.zeros((n,))
+    D = np.zeros((n,))
+    flx = np.zeros((n,))
     for i, line in enumerate(lines):
-        flx[i], sigT[i,i], D[i] = line.split()[:3]
-        sigS[i,:] = line.split()[3:]
+        flx[i], sigT[i], crap, D[i] = line.split()[:4]
+        sigS[i,:] = line.split()[4:]
     return sigT, sigS, flx, D
 
 def BCsolve(Up, Um, Dp, Dm, Jp, d):
     """Solves the boundary interface problems
 
+    Up = sqrt(sigA+/D), in 1/cm, where the + sign means it belongs to the right region
+    Um = Same as Up but for the left region
+    Dp/m = Diffusion coefficient in right/left region in cm
+    Jp = Left boundary condition
+    d = Left region width in cm
+
     """
+    
     exUp = expm(-d*Up)
     exUmp = expm(d*Um)
     exUmm = expm(-d*Um)
     n = exUp.shape[0]
-    b = np.zeros((3*n,1))
+    b = np.zeros((3*n,))
     for i,j in enumerate(range(2*n,3*n)):
         b[j] = Jp[i]
 
@@ -79,33 +53,33 @@ def BCsolve(Up, Um, Dp, Dm, Jp, d):
     v = linsolve(M,b)
     Ap = v[2*n:]
     return Ap,exUp
-            
 
-def solve(sigT, sigS, phi0, phi1, d, Jp):
+def solve(reg, d, Jp):
     """Solve for the diffusion coefficient using non-linear solvers
+
+    reg = Two region objects in a list
+    d = Length between regions in cm
+    Jp = Left boundary condition current
 
     """
     
-    phi0m, phi0p = phi0
-    phi1m, phi1p = phi1
+    phi0m, phi0p = reg[0].flx, reg[1].flx
+    phi1m, phi1p = reg[0].rJ, reg[1].rJ
 
     def residual(D):
         """Returns how close we are to convergence in each variable
 
         """
         n = D.shape[0]
-        Dm = np.zeros_like(sigT)
-        Dp = np.zeros_like(sigT)
-        for i in range(n):
-            Dm[i,i], Dp[i,i] = D[i,0], D[i,1]
-        Up = sqrtm(inv(Dp)*(sigT-sigS))
-        Um = sqrtm(inv(Dm)*(sigT-sigS))
+        Dm, Dp = np.diag(D[:,0]), np.diag(D[:,1])
+        Up = np.real(sqrtm(diag_inv(Dp).dot(reg[1].sigA)))
+        Um = np.real(sqrtm(diag_inv(Dm).dot(reg[0].sigA)))
         Ap, exU = BCsolve(Up,Um,Dp,Dm,Jp,d)
         rm = Dm.dot(phi0m) - Dm.dot(d*exU.dot(Ap)) - phi1m
         rp = Dp.dot(phi0p) + Dp.dot(d*exU.dot(Ap)) - phi1p
-        return np.concatenate(rm,rp)
+        return np.vstack((rm,rp)).T
     
-    guess = np.ones((Jp.shape[0],2))
+    guess = np.ones((phi0m.shape[0],2))
     return newton_krylov(residual, guess, method='lgmres')
     
 
@@ -113,15 +87,19 @@ if __name__ == '__main__':
     from argparse import ArgumentParser as AP
 
     parser = AP(description="Solves the very specific problem")
-    parser.add_argument('-i', default='data.dat', help="CMM tally results")
+    parser.add_argument('-i', nargs = 2, default=['leftreg.dat.csv', 'rightreg.dat.csv'], help="CMM tally results")
     parser.add_argument('-o', help="Output file")
 
     args = parser.parse_args()
 
-    inp = read_inp(args.i)
-    sigT, sigS, flx, D = inp
-    rJ = get_tallied(flx, D)
-    d = 1.0
-    Jp = np.zeros(flx.size)
+    regions = []
+    for f in args.i:
+        inp = read_inp(f)
+        sigT, sigS, flx, D = inp
+        rJ = get_tallied(flx, D)
+        regions.append(Region(sigT, sigS, flx, rJ))
+    d = 40.0
+    Jp = np.zeros_like(flx)
     Jp[0] = 1.0
-    D = solve(sigT, sigS, flx, rJ, d, Jp)
+    D = solve(regions, d, Jp)
+    print(D)
---
+++ util.py
@@ -0,0 +1,103 @@
+
+import numpy as np
+from scipy.sparse.linalg import aslinearoperator
+#from scipy.sparse.linalg import spsolve as solve
+from scipy.sparse.linalg import bicgstab as solve
+
+class BCCLS:
+    """Boundary conditions linear operator like object
+
+    """
+    def __init__(self, exUp, exUmp, exUmm, Up, Um, Dp, Dm, d):
+        self.exUp = exUp
+        self.exUmp = exUmp
+        self.exUmm = exUmm
+        self.Up = Up
+        self.Um = Um
+        self.Dp = Dp
+        self.Dm = Dm
+        self.d = d
+        self.n = Dm.shape[0]
+        self.shape = (3*self.n,3*self.n)
+        self.dtype = 'float64'
+        #self._test_()
+
+    def _test_(self):
+        """
+        print('Up')
+        print(self.Up)
+        print('Um')
+        print(self.Um)
+        print('Dp')
+        print(self.Dp)
+        print('Dm')
+        print(self.Dm)
+        """
+        print('exUp')
+        print(self.exUp)
+        
+
+    def matvec(self, v):
+        """Matrix vector multiplication
+
+        """
+        if v.shape[0] != 3*self.n:
+            raise ValueError("Moo!")
+        
+        n = self.n
+        sol = np.zeros_like(v)
+        v1,v2,v3 = v[:n],v[n:2*n],v[2*n:]
+        sol[:n] = self.exUmm.dot(v1) + self.exUmp.dot(v2) - self.exUp.dot(v3)
+        sol[n:2*n] = (self.Dm.dot(self.Um.dot(self.exUmp.dot(v2) - self.exUmm.dot(v1))) +
+                      self.Dp.dot(self.Up.dot(self.exUp.dot(v3))))
+        sol[2*n:] = v1 + v2 - (3.0*self.Dm.dot(self.Um.dot(v2 - v1)))
+        return sol
+    
+
+class Region:
+    """Data holding object for each region
+
+    """
+
+    def _check_fit(T,S):
+        """Check that T,S match sizes
+
+        """
+        n = T.shape[0]
+        if S.shape != (n,n) or len(T.shape) != 1:
+            raise ValueError("Matrix shapes don't align")
+        
+
+    def __init__(self, T, S, f, J):
+        Region._check_fit(T,S)
+        self.sigA = -S
+        n = S.shape[0]
+        for i in range(n):
+            self.sigA[i,i] += T[i]
+        self.flx = f
+        self.rJ = J
+        #self.__test__()
+
+    def __test__(self):
+        print(self.sigA)
+
+    
+def linsolve(M,b):
+    A = aslinearoperator(M)
+    v, info = solve(A,b,x0=b,tol=1e-6)
+    if info == 0:
+        return v
+    else:
+        print(info)
+
+
+def diag_inv(A):
+    """Invert the diagonal of a matrix, assumes no zeros.
+
+    """
+
+    B = A.copy()
+    n = A.shape[0]
+    for i in range(n):
+        B[i,i] = 1.0 / A[i,i]
+    return B
